e1cce3fd5bcb61b96700be2c6c0155270624c298
TeoAlex <teoalexsin@gmail.com>
2022-10-26 12:03:58 +0300

    [ #2 ] import/export

--- dlm-migrate-download-counts.php
+++ dlm-migrate-download-counts.php
@@ -28,6 +28,7 @@ class DLM_Migrate_Counts{
         if ( is_admin() ) {
             add_action( 'admin_notices', array( $this, 'admin_notices' ), 8 );
             add_action( 'admin_init', array( $this, 'action_handler' ) );
+            add_action( 'tool_box', array( $this, 'csv_export_import' ) );
         }
 
 	}
@@ -53,12 +54,13 @@ class DLM_Migrate_Counts{
                 <h2><?php esc_html_e( 'Download Monitor - Migrate Download Counts', 'dlm-migrate-counts' ); ?></h2>
                 <p><?php esc_html_e( 'Click the button below to migrate your Download Monitor\'s download counts. This is a one time only action.', 'dlm-migrate-counts' ); ?></p>
                 <p>
-                    <a href=" <?php echo add_query_arg( array( 'dlm_migrate_counts' => 1, 'nonce' => wp_create_nonce( 'dlm_mdc_nonce' ) ), get_admin_url() ); ?> "  class="button button-primary"><?php esc_html_e( 'Sync Manual Counts With Reports', 'dlm-migrate-counts' ); ?></a>
+                    <a href=" <?php echo add_query_arg( array( 'dlm_migrate_counts' => 1, 'dlm_mdc_nonce' => wp_create_nonce( 'dlm_mdc_nonce' ) ), get_admin_url() ); ?> "  class="button button-primary"><?php esc_html_e( 'Sync Manual Counts With Reports', 'dlm-migrate-counts' ); ?></a>
                 </p>
             </div>
             <?php
         }
-        if( isset( $_GET['dlm_migrate_success'] ) && '1' === $_GET['dlm_migrate_success'] ){
+
+        if( isset( $_REQUEST['dlm_migrate_success'] ) && '1' === $_REQUEST['dlm_migrate_success'] ){
             ?>
             <div id="dlm-migrate-download-counts-notice" class="notice notice-success" style="margin-top:30px;">
                 <h2><?php esc_html_e( 'Download Monitor - Migrate Download Counts', 'dlm-migrate-counts' ); ?></h2>
@@ -66,17 +68,26 @@ class DLM_Migrate_Counts{
             </div>
             <?php
         }
+        
+        if( isset( $_REQUEST['dlm_meta_import_success'] ) && '1' === $_REQUEST['dlm_meta_import_success'] ){
+            ?>
+            <div id="dlm-migrate-download-counts-notice" class="notice notice-success" style="margin-top:30px;">
+                <h2><?php esc_html_e( 'Download Monitor - Migrate Download Counts', 'dlm-migrate-counts' ); ?></h2>
+                <p><?php esc_html_e( 'Download Monitor\'s meta import was successfull.', 'dlm-migrate-counts' ); ?></p>
+            </div>
+            <?php
+        }
 	}
 
     public function action_handler(){
 
-        if( isset( $_GET['dlm_migrate_counts'] ) && '1' === $_GET['dlm_migrate_counts'] ){
+        if( isset( $_REQUEST['dlm_migrate_counts'] ) && '1' === $_REQUEST['dlm_migrate_counts'] ){
            
             if( get_option( 'dlm_mdc_ran', false) ){
                 return false;
             }
 
-            if ( empty( $_REQUEST['nonce'] ) || ! wp_verify_nonce( wp_unslash( $_REQUEST['nonce'] ), 'dlm_mdc_nonce' ) ) {
+            if ( empty( $_REQUEST['dlm_mdc_nonce'] ) || ! wp_verify_nonce( wp_unslash( $_REQUEST['dlm_mdc_nonce'] ), 'dlm_mdc_nonce' ) ) {
                 return false;
             }
 
@@ -117,11 +128,150 @@ class DLM_Migrate_Counts{
             wp_redirect( add_query_arg( 'dlm_migrate_success', 1, get_admin_url() ) );
             exit;
             
+        }elseif( isset( $_REQUEST['dlm_mdc_action'] ) && 'export' === $_REQUEST['dlm_mdc_action'] ){
+
+            $this->csv_export();
+        }elseif( isset( $_REQUEST['dlm_mdc_action'] ) && 'import' === $_REQUEST['dlm_mdc_action'] ){
+
+            $this->csv_import();
         }
     }
 
+    public function csv_export_import(){
+        ?>
+        <div class="card">
+			<h2 class="title"><?php esc_html_e( 'Download Monitor - Migrate Download Counts', 'dlm-migrate-counts' ); ?></h2>
+			<div>
+                <p>Export DLM downloads meta</p>
+                <a href=" <?php echo add_query_arg( array( 'dlm_mdc_action' => 'export', 'dlm_mdc_nonce' => wp_create_nonce( 'dlm_mdc_nonce' ) ), get_admin_url() . 'tools.php' ); ?> "  class="button button-primary"><?php esc_html_e( 'Export to CSV', 'dlm-migrate-counts' ); ?></a>
+            </div>
+			<div>
+                <form enctype="multipart/form-data" method="POST" action="<?php echo get_admin_url() . 'tools.php'; ?>">
+                <p> or import DLM downloads meta</p>
+                <input type="hidden" value="<?php echo wp_create_nonce( 'dlm_mdc_nonce' ); ?>" name="dlm_mdc_nonce" />
+                <input type="hidden" value="import" name="dlm_mdc_action" />
+                <input type="file" name="dlm_migrate_counts_csv" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"/><br>
+                <button class="button button-primary"><?php esc_html_e( 'Import from CSV', 'dlm-migrate-counts' ); ?></button>
+            </div>
+		</div>
+        <?php
+    }
+
+    public function csv_export(){
+
+        if ( ! current_user_can( 'manage_options' ) ) {
+            wp_die( 'Aborted export request, insufficient user permission to export download data.' );
+        }
+
+        // Check nonce
+        if ( ! wp_verify_nonce( $_REQUEST['dlm_mdc_nonce'], 'dlm_mdc_nonce' ) ) {
+            wp_die( 'Aborted export request, nonce check failed.' );
+        }
+
+		// Build array
+		$csv_data = array();
+
+		// Add CSV header
+		$csv_data['header'] = array(
+			'download_id',
+			'_download_count',
+		);
+
+		$csv_string = '';
+
+		// Headers
+		$csv_string .= implode( ',', $csv_data['header'] ) . PHP_EOL;
+
+        // Loop
+        global $wpdb;
+
+        $downloads = $wpdb->get_results( "SELECT download_id FROM " . $wpdb->download_log . " GROUP BY download_id LIMIT 0, 999999", 'ARRAY_A' );
+    
+        foreach( $downloads as $download ){
+            $download_count = get_post_meta( $download['download_id'], '_download_count', true );
+
+            $csv_string .= ( isset( $download['download_id'] ) ? $download['download_id'] : 0 ). ',' . ( $download_count ? $download_count : '0' ) . PHP_EOL;
+
+        }
+
+        // Check
+        if ( '' !== $csv_string ) {
+
+            // Ouput the CSV headers
+            $this->output_headers();
+
+            // Output the string
+            echo $csv_string;
+            exit;
+
+        } else {
+            wp_die( 'Download Monitor export failed, no data in CSV string found.' );
+        }
+    }
+
+    /**
+     * Output the CSV headers
+     */
+    public function output_headers() {
+        header( "Content-type: text/csv" );
+        header( "Content-Disposition: attachment; filename=download-monitor-download-counts.csv" );
+        header( "Pragma: no-cache" );
+        header( "Expires: 0" );
+    }
+
+    private function csv_import(){
+
+        if ( ! current_user_can( 'manage_options' ) ) {
+            wp_die( 'Aborted import request, insufficient user permission to import download data.' );
+        }
+
+        if ( ! wp_verify_nonce( $_REQUEST['dlm_mdc_nonce'], 'dlm_mdc_nonce' ) ) {
+            wp_die( 'Aborted import request, nonce check failed.' );
+        }
+
+        if( !isset( $_FILES['dlm_migrate_counts_csv']['tmp_name'] ) || '' === $_FILES['dlm_migrate_counts_csv']['tmp_name'] ){
+            wp_die( 'Aborted import request, CSV file not imported.' );
+        }
+
+        $tmpName = $_FILES['dlm_migrate_counts_csv']['tmp_name'];
+        $rows = array_map('str_getcsv', file( $tmpName ) );
+        $header = array_shift( $rows );
+
+        foreach( $rows as $row ) {
+            
+            $metas = array_combine($header, $row);
+
+            // No download id: skip.
+            if( 0 === absint( $metas['download_id'] ) ){ continue; }
+
+            $check_meta = get_post_meta( $metas['download_id'], '_download_count', true );
+
+            if( $check_meta ){
+
+                // Download count is 0, we delete the meta.
+                if( 0 === absint( $metas['_download_count'] ) ){
+
+                    delete_post_meta( $metas['download_id'], '_download_count' );
+                }else{
+
+                    update_post_meta( $metas['download_id'], '_download_count', $metas['_download_count'] );
+                }
+            }else{
+
+                if( 0 !== absint( $metas['_download_count'] ) ){
+
+                    add_post_meta( $metas['download_id'], '_download_count', $metas['_download_count']  );
+                }
+            }
+
+        }
+
+        wp_redirect( add_query_arg( 'dlm_meta_import_success', 1, get_admin_url() . 'tools.php' ) );
+    }
 
 }
+
+
 
 function _dlm_mdc() {
 
